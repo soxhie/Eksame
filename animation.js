844822955756f7833ae79f5e8323e2e2ef89ffea
soxhie <sohj0001@stud.kea.dk>
2024-01-09 21:35:25 +0100

    footer correction

---
+++ animation.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  
+
+
+  const burgerMenu = document.querySelector('.burger-menu');
+  const topMenu = document.querySelector('.topmenu')
+
+  burgerMenu.addEventListener('click', function (){
+    topMenu.classList.toggle('show');
+  });
+
+    
+  
+
+});
+
+
+
+
+  
---
+++ billet.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const backgroundImage = document.querySelector('.background-image');
+
+    // Slide the background image into position when the DOM is loaded
+    backgroundImage.style.top = '0';
+    
+});
