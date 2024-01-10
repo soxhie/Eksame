570e66189b248a017dbf715398a93ad13881e4ad
soxhie <sohj0001@stud.kea.dk>
2024-01-10 03:57:45 +0100

    everything good

--- billet.js
+++ billet.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const backgroundImage = document.querySelector('.background-image');
 
-    // Slide the background image into position when the DOM is loaded
+    // Slide the background image into position when page is loaded
     backgroundImage.style.top = '0';
     
 });
